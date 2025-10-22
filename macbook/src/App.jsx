05424d3a550de36c6db6a7d6b795b45883e61915
Saurabh <risabht043@gmail.com>
2025-10-22 21:29:20 +0530

    till featres done

--- macbook/src/App.jsx
+++ macbook/src/App.jsx
@@ -5,6 +5,8 @@ import ProductViewer from './components/ProductViewer'
 import { gsap } from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Showcase from './components/ShowCase'
+import Performance from './components/Performance'
+import Features from './components/Features'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -15,6 +17,8 @@ const App = () => {
             <Hero/>
             <ProductViewer/>
             <Showcase/>
+            <Performance/>
+            <Features/>
         </main>
     )
 }
